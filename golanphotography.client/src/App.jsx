476bb810644ffa933af5afc1d0146ca8d9e65038
Truueh <zoritn@gmail.com>
2024-05-13 18:27:41 +0300

    Added an introduction area under the landing area - some introduction text with a profile picture.

--- golanphotography.client/src/App.jsx
+++ golanphotography.client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
             <Head />
             <div id="home_body">
                 <LandingArea />
+                <IntroductionArea />
             </div>
             <Footer />
         </div>
@@ -25,6 +26,23 @@ function LandingArea() {
     return (
         <img className="background-image" src= "../Images/HomePage/LandingImages/1.png" />
     );
+}
+
+function IntroductionArea() {
+    return (
+        <div>
+            <div className="breaker" id="breaker_1"></div>
+            <div id="introduction-area">
+                <div id="introduction-text">
+                    <h2 id="introduction-title">!היי<br></br>(: אני גולן ואני צלם</h2>
+                    <p id="introduction-paragraph">מתמחה בצילום באמצעות פאלפונים ניידים<br></br>
+                        בין היתר מעביר סדנאות ומנהל קבוצת<br></br>
+                    פייסבוק לצלמים מהמובילות בארץ בתחום</p>
+                </div>
+                <img id="profile-picture" src="../Images/HomePage/profile.jpg"></img>
+            </div>
+        </div>
+    )
 }
 
 function Footer() {
