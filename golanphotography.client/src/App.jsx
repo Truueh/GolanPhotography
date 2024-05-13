989d7b98ec0c1c61fac181b23d6d03e2f4952be7
Truueh <zoritn@gmail.com>
2024-05-14 02:13:41 +0300

    Added a first version of the images brick pattern (under introduction area)

--- golanphotography.client/src/App.jsx
+++ golanphotography.client/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
             <div id="home_body">
                 <LandingArea />
                 <IntroductionArea />
+                <BrickPattern />
             </div>
             <Footer />
         </div>
@@ -21,9 +22,9 @@ function Head() {
             <h1 id="title"> GOLAN PHOTOGRAPHY </h1>
 
             <div id="navigation-menu" className="navigation-menu">
-                <h1 className="navigation-item"> conact us </h1>
-                <h1 className="navigation-item"> meetups </h1>
-                <h1 className="navigation-item"> about </h1>
+                <h1 className="navigation-item"> צרו קשר </h1>
+                <h1 className="navigation-item"> מפגשים </h1>
+                <h1 className="navigation-item"> אודות </h1>
             </div>
         </div>
     );
@@ -42,11 +43,39 @@ function IntroductionArea() {
             <div id="introduction-area">
                 <div id="introduction-text">
                     <h2 id="introduction-title">!היי<br></br>(: אני גולן ואני צלם</h2>
-                    <p id="introduction-paragraph">מתמחה בצילום באמצעות פאלפונים ניידים<br></br>
-                        בין היתר מעביר סדנאות ומנהל קבוצת<br></br>
-                    פייסבוק לצלמים מהמובילות בארץ בתחום</p>
+                    <p id="introduction-paragraph">.מתמחה בצילום באמצעות פאלפונים ניידים
+                        בין היתר מעביר סדנאות ומנהל קבוצת
+                        פייסבוק לצלמים מהמובילות בארץ בתחום
+                    </p>
                 </div>
                 <img id="profile-picture" src="../Images/HomePage/profile.jpg"></img>
+            </div>
+        </div>
+    )
+}
+
+function BrickPattern() {
+    return (
+        <div id="brick-pattern-div">
+            <div id="brick-pattern-col-1" class="brick-pattern-col">
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+            </div>
+            <div id="brick-pattern-col-2" class="brick-pattern-col">
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+            </div>
+            <div id="brick-pattern-col-3" class="brick-pattern-col">
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+            </div>
+            <div id="brick-pattern-col-4" class="brick-pattern-col">
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
             </div>
         </div>
     )
