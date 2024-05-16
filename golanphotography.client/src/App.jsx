6d8e141b4275e06b49d55160b10403c0605c28ce
Truueh <zoritn@gmail.com>
2024-05-16 19:05:15 +0300

    Changed landing image behaviour to not stretch.
    Changed brick patterned images behaviour to scale instead of hard coded enlargement.
    
    Added initial phase of image inspection mode.

--- golanphotography.client/src/App.jsx
+++ golanphotography.client/src/App.jsx
@@ -1,9 +1,11 @@
 ﻿import { useEffect, useState } from 'react';
 import './App.css';
+import './jquery.js'
 
 function App() {
     return (
         <div>
+            <div id="inspect-image-mode-div" onClick={inspectModeClick}></div>
             <Head />
             <div id="home_body">
                 <LandingArea />
@@ -32,7 +34,7 @@ function Head() {
 
 function LandingArea() {
     return (
-        <img className="background-image" src= "../Images/HomePage/LandingImages/1.png" />
+        <img className="background-image" src= "../Images/HomePage/LandingImages/3.jpg" />
     );
 }
 
@@ -42,7 +44,7 @@ function IntroductionArea() {
             <div className="breaker" id="breaker_1"></div>
             <div id="introduction-area">
                 <div id="introduction-text">
-                    <h2 id="introduction-title">!היי<br></br>(: אני גולן ואני צלם</h2>
+                    <h2 id="introduction-title">היי<br></br> אני גולן ואני צלם</h2>
                     <p id="introduction-paragraph">.מתמחה בצילום באמצעות פאלפונים ניידים
                         בין היתר מעביר סדנאות ומנהל קבוצת
                         פייסבוק לצלמים מהמובילות בארץ בתחום
@@ -56,26 +58,26 @@ function IntroductionArea() {
 
 function BrickPattern() {
     return (
-        <div id="brick-pattern-div">
-            <div id="brick-pattern-col-1" class="brick-pattern-col">
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+        <div id="brick-pattern-div" onLoad={applyClickListener }>
+            <div id="brick-pattern-col-1" className="brick-pattern-col">
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/3.jpg"></img>
             </div>
-            <div id="brick-pattern-col-2" class="brick-pattern-col">
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+            <div id="brick-pattern-col-2" className="brick-pattern-col">
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/1.jpg"></img>
             </div>
-            <div id="brick-pattern-col-3" class="brick-pattern-col">
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+            <div id="brick-pattern-col-3" className="brick-pattern-col">
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/2.jpg"></img>
             </div>
-            <div id="brick-pattern-col-4" class="brick-pattern-col">
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/3.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/2.jpg"></img>
-                <img className="brick-pattern-img" src="../Images/HomePage/BrickPattern/1.jpg"></img>
+            <div id="brick-pattern-col-4" className="brick-pattern-col">
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/3.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/2.jpg"></img>
+                <img className="brick-pattern-img inspectable-image" src="../Images/HomePage/BrickPattern/1.jpg"></img>
             </div>
         </div>
     )
@@ -87,6 +89,55 @@ function Footer() {
             <h6>footer bitch</h6>
         </div>
     )
+}
+
+function applyClickListener() {
+    var element = document.getElementsByClassName("inspectable-image");
+
+    for (var i = 0; i < element.length; i++) {
+        element[i].addEventListener('click', inspectImage);
+    }
+}
+
+function inspectImage() {
+    toggleDarkBackground(true);
+    insertInspectImg(event);
+    toggleBlur();
+}
+
+function insertInspectImg(event) {
+    let img = document.createElement("img");
+    img.src = event.target.src;
+    img.id = "inspected-img";
+    document.getElementById('inspect-image-mode-div').appendChild(img);
+}
+
+function inspectModeClick() {
+    leaveInspectMode();
+}
+
+function leaveInspectMode() {
+    toggleDarkBackground(false);
+    removeInspectImg();
+    toggleBlur();
+}
+
+
+function removeInspectImg() {
+    document.getElementById('inspected-img').remove();
+}
+
+function toggleDarkBackground(toggle) {
+    if (toggle)
+        $("#inspect-image-mode-div").css("visibility", "visible")
+    else {
+        $("#inspect-image-mode-div").css("visibility", "hidden")
+    }
+}
+
+function toggleBlur(toggle) {
+    let background = document.getElementById('home_body');
+    background.classList.toggle('blur');
 }
 
 export default App;
